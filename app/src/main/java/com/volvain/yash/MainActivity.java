14009927a647654fe7c09a76df4017e5c797d578
Kartik <kartik198200023@gmail.com>
2019-08-14 15:28:36 +0530

    scroll bar

--- app/src/main/java/com/volvain/yash/MainActivity.java
+++ app/src/main/java/com/volvain/yash/MainActivity.java
@@ -3,9 +3,17 @@ package com.volvain.yash;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
+
+
+
+
+
+}
